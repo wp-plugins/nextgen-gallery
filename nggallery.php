9439d7f8bcbb4d506520414faf6309f5e67e48ad
alexrabe <alexrabe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-04-28 09:33:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nextgen-gallery/trunk@11788 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nggallery.php
+++ nggallery.php
@@ -4,7 +4,7 @@ Plugin Name: NextGEN Gallery
 Plugin URI: http://alexrabe.boelinger.com/?page_id=80
 Description: A NextGENeration Photo gallery for the WEB2.0(beta). At the moment only poor Web1.0 :-(
 Author: Alex Rabe
-Version: 0.37a
+Version: 0.38a
 
 Author URI: http://alexrabe.boelinger.com/
 
@@ -42,7 +42,7 @@ global $wpdb, $wp_version;
 if (version_compare($wp_version, '2.1', '>=')) {
 
 // Version and path to check version
-define('NGGVERSION', "0.37");
+define('NGGVERSION', "0.38");
 define('NGGURL', "http://nextgen.boelinger.com/version.php");
 
 // define URL
